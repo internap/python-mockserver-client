353379a18012b7fa2822f53142a98773c6ef6ed2
Martin Roy <mroy@internap.com>
2018-06-28 09:29:46 -0400

    Slow down calls to mock server :|

--- test/__init__.py
+++ test/__init__.py
@@ -1,8 +1,15 @@
+import time
 import unittest
 
 from mockserver import MockServerClient
 
 MOCK_SERVER_URL = "http://localhost:1080"
+
+
+class SlowMockServerClient(MockServerClient):
+    def _call(self, command, data=None):
+        time.sleep(0.01)
+        return super()._call(command, data)
 
 
 class MockServerClientTestCase(unittest.TestCase):
