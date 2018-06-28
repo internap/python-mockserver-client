23260866b3c285fd6e41b00695213de5117aaf3b
Martin Roy <mroy@internap.com>
2018-06-28 11:29:18 -0400

    stub that 404

--- test/test_basic_verifying.py
+++ test/test_basic_verifying.py
@@ -7,6 +7,7 @@ from test import MOCK_SERVER_URL, MockServerClientTestCase
 
 class TestBasicVerifying(MockServerClientTestCase):
     def test_verify_request_received_once(self):
+        self.client.stub(request(), response())
         result = requests.get(MOCK_SERVER_URL)
         logging.info("Elapsed {}".format(result.elapsed))
         self.client.verify(request(), times(1))
